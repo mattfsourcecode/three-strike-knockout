1f67dbd27a37897f57fc7c6dfcd625f9e0352faa
Matt Eric <mf9646a@alumni.american.edu>
2020-12-03 14:51:34 -0500

    Configure pug

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
- 
+import * as path from 'path';
+
 const app = express();
- 
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use(cors());
  
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.render('index');
 });
  
 app.listen(3000, () =>
