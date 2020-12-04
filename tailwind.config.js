8ea81b13edcdeb4e421f8915f81a0ebd6a2f0c3d
Matt Eric <mf9646a@alumni.american.edu>
2020-12-04 00:08:39 -0500

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883'
+        },
+        tailwind: '#06B6D4'
+      },
+      fontFamily: {
+        body: ['Permanent Marker']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
