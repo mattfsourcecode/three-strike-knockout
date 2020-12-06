3543c8e781c67babc26cd1f94cd545ac1325473d
Matt Eric <mf9646a@alumni.american.edu>
2020-12-05 19:35:21 -0500

    Adjust chip pyramid restitution

--- public/index.js
+++ public/index.js
@@ -203,7 +203,7 @@
     
         const successChipPyramid = Composites.pyramid(600, -50, 12, 13, 0, 0, function(x, y) {
             return Bodies.circle(x, y, 10, {
-                restitution: 1.4,
+                restitution: 1,
                 render: {
                     sprite: {
                       texture: chips[getRandomInt(chips.length)],
