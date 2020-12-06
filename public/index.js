7b562c6b1b65081a6a4dcafe83dd5e3701d30ba5
Matt Eric <mf9646a@alumni.american.edu>
2020-12-05 22:52:23 -0500

    Remove pyramid from "remove" method on game win

--- public/index.js
+++ public/index.js
@@ -199,7 +199,7 @@
     // call when conditions are met such that the game has been won
     const startWinAnimation = () => {
 
-        World.remove(engine.world, [ground, pyramid]);
+        World.remove(engine.world, [ground]);
 
         const successCardPyramid = Composites.pyramid(500, -200, 19, 20, 0, 0, function(x, y) {
             return Bodies.rectangle(x, y, 25, 35, {
