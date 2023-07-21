64e9fbc41a42d98c801f6b1cd3c4313c98a3e33b
Matt Fagan <24415914+matt-development-work@users.noreply.github.com>
2023-07-20 18:47:35 -0600

    Update styles

--- dist/client.js
+++ dist/client.js
@@ -48,6 +48,31 @@ function _unsupported_iterable_to_array(o, minLen) {
 }
 import $ from "jquery";
 import Matter from "matter-js";
+navigator.isCompatible = function() {
+    var getBrowser = function() {
+        var userAgent = navigator.userAgent, tem, M = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+        if (/trident/i.test(M[1])) {
+            tem = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
+            return "IE " + (tem[1] || "");
+        }
+        if (M[1] === "Chrome") {
+            tem = userAgent.match(/\b(OPR|Edge?)\/(\d+)/);
+            if (tem != null) return tem.slice(1).join(" ").replace("OPR", "Opera").replace("Edg ", "Edge ");
+        }
+        M = M[2] ? [
+            M[1],
+            M[2]
+        ] : [
+            navigator.appName,
+            navigator.appVersion,
+            "-?"
+        ];
+        if ((tem = userAgent.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+        return M.join(" ");
+    };
+    var browser = getBrowser();
+    return browser.includes("Chrome") || browser.includes("Opera");
+}();
 (function() {
     if (!navigator.isCompatible) {
         return $("#spinner-container-inner").html('<h2 class="header"><div class="mb-10">This browser or device is not compatible yet.</div><div class="mb-10">Please use Chrome or Opera on a computer to enjoy</div><div class="mb-10">\uD83C\uDCCF Three-strike Knockout \uD83C\uDF89.</div></h2>');
--- public/client.js
+++ public/client.js
@@ -11175,6 +11175,28 @@ var require_matter = __commonJS({
 // src/client.js
 var import_jquery = __toESM(require_jquery());
 var import_matter_js = __toESM(require_matter());
+navigator.isCompatible = (() => {
+  const getBrowser = () => {
+    let userAgent = navigator.userAgent, tem, M = userAgent.match(
+      /(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i
+    ) || [];
+    if (/trident/i.test(M[1])) {
+      tem = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
+      return "IE " + (tem[1] || "");
+    }
+    if (M[1] === "Chrome") {
+      tem = userAgent.match(/\b(OPR|Edge?)\/(\d+)/);
+      if (tem != null)
+        return tem.slice(1).join(" ").replace("OPR", "Opera").replace("Edg ", "Edge ");
+    }
+    M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, "-?"];
+    if ((tem = userAgent.match(/version\/(\d+)/i)) != null)
+      M.splice(1, 1, tem[1]);
+    return M.join(" ");
+  };
+  const browser = getBrowser();
+  return browser.includes("Chrome") || browser.includes("Opera");
+})();
 (() => {
   if (!navigator.isCompatible) {
     return (0, import_jquery.default)("#spinner-container-inner").html(
